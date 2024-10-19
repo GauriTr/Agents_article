e9c801d6ce7307fdf0e053e78c0f8c19e9ee2ce2
GauriTr <gauritr19@gmail.com>
2024-10-19 21:54:29 +0530

    setting crew

--- webportcrew/src/webportcrew/crew.py
+++ webportcrew/src/webportcrew/crew.py
@@ -1,51 +1,88 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+import os, yaml, sys
+from crewai_tools import DirectoryReadTool, FileReadTool,CodeInterpreterTool, DOCXSearchTool
 
-# Uncomment the following line to use an example of a custom tool
-# from webportcrew.tools.custom_tool import MyCustomTool
 
-# Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
+
+llm1 ='groq/llama-3.1-70b-versatile'
+llm2 = 'mistral/codestral-latest'
+
+docs_tool = DirectoryReadTool()
+code = CodeInterpreterTool()
+tools = [docs_tool, code]
+
+
 
 @CrewBase
 class WebportcrewCrew():
 	"""Webportcrew crew"""
 
+	def __init__(self):
+		self.original_agents_config_path = r'config/agents.yaml'
+		self.original_tasks_config_path = r'config/tasks.yaml'
+		
+		with open(r'config/agents.yaml', 'r') as f:
+			self.agents_config = yaml.safe_load(f)
+		with open(r'config/tasks.yaml', 'r') as f:
+			self.tasks_config = yaml.safe_load(f)
+   
 	@agent
-	def researcher(self) -> Agent:
+	def ui_ux_designer(self) -> Agent:
 		return Agent(
-			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-			verbose=True
+			config=self.agents_config['ui_ux_designer'],
+			tools=tools, 
+			verbose=True,
+			llm=llm1,
+			allow_code_execution=True
 		)
 
 	@agent
-	def reporting_analyst(self) -> Agent:
+	def software_developer(self) -> Agent:
 		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
+			config=self.agents_config['software_developer'],
+			verbose=True,
+			llm=llm1,
+			tools=tools,
+			allow_code_execution=True
 		)
+
 
 	@task
-	def research_task(self) -> Task:
+	def ui_ux_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['research_task'],
+			config=self.tasks_config['ui_ux_task'],
+			agent=self.ui_ux_designer(),
+			output_file='ui_ux_design.md'
 		)
-
 	@task
-	def reporting_task(self) -> Task:
+	def html_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
+			config=self.tasks_config['html_task'],
+			agent=self.software_developer(),
+			output_file='index.html'
+		)
+	@task
+	def css_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['css_task'],
+			agent=self.software_developer(),
+			output_file='styles/styles.css'
+		)
+	@task
+	def js_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['js_task'],
+			agent=self.software_developer(),
+			output_file='scripts/script.js'
 		)
 
 	@crew
 	def crew(self) -> Crew:
-		"""Creates the Webportcrew crew"""
+		"""Creates the SimplePort crew"""
 		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
+			agents=self.agents, 
+			tasks=self.tasks, 
 			process=Process.sequential,
 			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
